c9b2b51309a09f8d9214a554d5d33b25e9ba1ffc
Westerbly Snaydley <westerbly@gmail.com>
2017-02-15 14:05:07 -0500

    outlined the first iteration of the game

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,5 +1,32 @@
+#include <iostream>
+#include <cstdint>
+
 #include <SDL2/SDL.h>
 #include <GL/gl3w.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cpplog/cpplog.hpp>
+
+cpplog::StdErrLogger logger;
+
+float GetDeltaTime() {
+    static uint64_t lastMeasure = 0;
+    uint64_t currentMeasure = SDL_GetPerformanceCounter();
+    uint64_t delta = currentMeasure - lastMeasure;
+    lastMeasure = currentMeasure;
+
+    return (delta) / (float)SDL_GetPerformanceFrequency();
+}
+
+void GameUpdate(float dt) {
+
+}
+
+void GameDraw() {
+
+}
+
 
 int main(int argc, char** argv) {
     /* Setup window and OpenGL context */
@@ -15,27 +42,45 @@ int main(int argc, char** argv) {
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
     //TODO: make resolution configurable
+    //TODO: Add SDL error handling / logging
     SDL_Window *window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     SDL_GLContext glcontext = SDL_GL_CreateContext(window);
     gl3wInit();
     glEnable(GL_MULTISAMPLE);
 
+    glm::vec4 clearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
     /* Main loop */
     bool done = false;
+    SDL_Event event;
+    float deltaTime = GetDeltaTime(); //needs to be called once before the loop so we discard the initialization time.
     while (!done) {
-        SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                //TODO: it's suggested to handle the QUIT event, or add code after the main loop to handle the exit.
-                done = true;
+            switch (event.type) {
+                case SDL_QUIT:
+                    //TODO: it's suggested to handle the QUIT event, or add code after the main loop to handle the exit.
+                    done = true;
+                    break;
+                case SDL_KEYDOWN:
+                    break;
+                case SDL_KEYUP:
+                    break;
+                default:
+                    break;
             }
         }
 
+        GameUpdate(deltaTime);
+
         /* Basic rendering maintenance */
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f); //TODO: parametrize Clear Color
+        glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        GameDraw();
+        
         SDL_GL_SwapWindow(window);
 
+        //let the processor catch a breath
         SDL_Delay(1);
     }
 
